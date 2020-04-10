414839d1850ca05e475bc40000f2aaa2b8fa4c77
sk <snpkeeley@gmail.com>
2020-04-10 13:25:08 -0700

    Deleted .env

--- src/app/components/MushroomCard/index.js
+++ src/app/components/MushroomCard/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import _ from 'lodash';
 import "./styles.scss";
 import {
   Card
@@ -13,7 +14,7 @@ const MushroomCard = ({mushroom}) => {
         image={mushroom.default_photo.square_url.replace("square", "large")}
         className="mushroomCard"
         header={mushroom.name}
-        meta={mushroom.preferred_common_name ? mushroom.preferred_common_name : ""}
+        meta={mushroom.preferred_common_name ?  _.upperFirst(mushroom.preferred_common_name) : ""}
       />
     </Link>
   );
