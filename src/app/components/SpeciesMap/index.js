87956479c007e65afd301d05f7cb71e5a9dbc4f6
snkly <800680+snkly@users.noreply.github.com>
2020-04-23 16:03:30 -0700

    Tidied up map component

--- src/app/components/SpeciesMap/index.js
+++ src/app/components/SpeciesMap/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import "./styles.scss";
-import { Grid, Image, Segment } from 'semantic-ui-react'
+import { Grid, Image, Segment } from 'semantic-ui-react';
 
-// Hex: &bin=hex&hexPerTile=80
-// Squares: &bin=square&squareSize=64
-// Styles: green.poly, green2.poly, classic.poly, classic-noborder.poly, orange.marker, scaled-circles
 const SpeciesMap = ({ mapKey }) => {
+  const gbifMap = (z, x, y) => (
+    <Image 
+      fluid
+      src={mapKey ? `https://api.gbif.org/v2/map/occurrence/density/${z}/${x}/${y}@1x.png?srs=EPSG:4326&style=classic-noborder.poly&taxonKey=${mapKey}&bin=hex&hexPerTile=75` : null }
+      onError={(e) => {e.target.onerror = null; e.target.src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="} }
+    />  
+  )
+
   return (
     <Segment className="speciesMap">
       <Grid columns={2}>
         <Grid.Column>
-          <Image fluid src={`https://api.gbif.org/v2/map/occurrence/density/0/0/0@1x.png?srs=EPSG:4326&style=classic-noborder.poly&taxonKey=${mapKey}&bin=hex&hexPerTile=75`}/>
+          {gbifMap(0, 0, 0)}
         </Grid.Column>
         <Grid.Column>
-          <Image fluid src={`https://api.gbif.org/v2/map/occurrence/density/0/1/0@1x.png?srs=EPSG:4326&style=classic-noborder.poly&taxonKey=${mapKey}&bin=hex&hexPerTile=75`} />
+          {gbifMap(0, 1, 0)}
         </Grid.Column>
       </Grid>
     </Segment>
